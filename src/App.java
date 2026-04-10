2092c7d9478e643314f7327802579d770d0e0fe1
Stephan Cedillo <stephancedillo@MacBook-Air-de-Stephan.local>
2026-04-10 10:46:05 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int number = 5;
+        int[] array = new int[] {1, 2, 3, 4, 5};
+        System.out.println("Numero: " + number);
+        System.out.print("Arreglo:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        
     }
 }
